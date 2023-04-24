900ed12c2ff1c5ddab93414d0f96e9dbace1dc1b
Axelo21 <axel.osterberg@skola.taby.se>
2023-04-24 08:50:43 +0200

    version 0.2.1 (minor tweaks)

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ canvas.height = window.innerHeight
 let context = canvas.getContext("2d")
 
 context.fillStyle = "black"
-context.fillRect(10, 10, canvas.width, canvas.height)
+context.fillRect(0, 0, canvas.width, canvas.height)
 
 player = {
     color: "red",
@@ -19,3 +19,21 @@ function drawRect(rect){
     context.fillStyle = rect.color
     context.fillRect(rect.posX, rect.posY, rect.width, rect.height)
 }
+
+function clearScreen(){
+    context.fillStyle = "black"
+    context.fillRect(10, 10, canvas.width, canvas.height)
+}
+
+function moveObject(object){
+    object.posX += 1
+    object.posY += 1
+}
+
+function update(){
+    moveObject(player)
+    drawRect(player)
+    clearScreen()
+}
+
+setInterval(update, 10)
