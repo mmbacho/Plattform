df0394df7dccbcbac49e1fe3db59b34fabd44fa8
Axelo21 <axel.osterberg@skola.taby.se>
2023-05-19 23:18:03 +0200

    Version 1.0 (Final)

--- game.js
+++ game.js
@@ -1,267 +1,335 @@
-//Klasser
-class Player{
-    constructor(tag, color, width, height, posX, posY, gravity, jumpSpeed, numOfJumps, jumpsAllowed, image){
-        this.tag = "player"
-        this.color = color
-        this.width = width
-        this.height = height
-        this.posX = posX
-        this.posY = posY
-        this.gravity = gravity
-        this.gravitySpeed = 0
-        this.jumpSpeed = jumpSpeed
-        this.speedY = 0
-        this.numOfJumps = numOfJumps
-        this.jumpsAllowed = jumpsAllowed
-        this.score = 0
-        this.image = image
-    }
+//Classes
+class Player {
+    constructor(width, height, posX, posY, gravity, jumpSpeed, numOfJumps, jumpsAllowed, image) {
+    this.width = width;
+    this.height = height;
+    this.posX = posX;
+    this.posY = posY;
+    this.gravity = gravity;
+    this.gravitySpeed = 0;
+    this.jumpSpeed = jumpSpeed;
+    this.speedY = 0;
+    this.numOfJumps = numOfJumps;
+    this.jumpsAllowed = jumpsAllowed;
+    this.score = 0;
+    this.image = image;
+  }
 }
 
-
-class Platform{
-    constructor(tag, color, width, height, posX, posY){
-        this.tag = "platform"
-        this.color = color
-        this.width = width
-        this.height = height
-        this.posX = posX
-        this.posY = posY
-        this.speedX = -0.5
-    }
+class Platform { 
+    constructor(width, height, posX, posY, image) {
+    this.width = width;
+    this.height = height;
+    this.posX = posX;
+    this.posY = posY;
+    this.speedX = -0.5;
+    this.image = image;
+    this.isGivenPoint = false;
+  }
 }
-// canvas.width = window.innerWidth
-// canvas.height = window.innerHeight
 
-let canvas = document.getElementById("canvas")
-canvas.width = 1000
-canvas.height = 500
+let canvas = document.getElementById("canvas");
+canvas.width = 1000;
+canvas.height = 500;
 
-yCenterCoord = canvas.height / 2
-xCenterCoord = canvas.width / 2
+yCenterCoord = canvas.height / 2;
+xCenterCoord = canvas.width / 2;
 
-let context = canvas.getContext("2d")
+let context = canvas.getContext("2d");
 
-const img = new Image()
-img.src = "background.png"
+const platformImage = new Image();
+platformImage.src = "platform.png";
+
+const img = new Image();
+img.src = "background.png";
 img.onload = () => {
-    context.drawImage(img, 0,0)
-}
-context.fillRect(0, 0, canvas.width, canvas.height)
+  context.drawImage(img, 0, 0);
+};
+context.fillRect(0, 0, canvas.width, canvas.height);
 
-platformDefaultSpawnLevel = yCenterCoord + 100
 
-let isOnGround = false
+//Game area
+const platformSpawnX = canvas.width - 300;
+const endOfScreen = -100;
+const groundLevel = canvas.height
+const roofLevel = 0;
+
+let highscore = 0;
 let isGameOver = false
-const platformSpawnX = canvas.width - 300
-const endOfScreen = -100
-const groundLevel = canvas.height-100
-let roofLevel = 0
 
-let highscore = 0
+//Game UI
+let scoreDiv = document.getElementById("score");
+let highscoreDiv = document.getElementById("highscore");
+let game = document.getElementById("game_elements");
+let menu = document.getElementById("menu_elements");
 
-let scoreDiv = document.getElementById("score")
-let highscoreDiv = document.getElementById("highscore")
+//Player var
+const spawnpointX = xCenterCoord;
+const spawnpointY = yCenterCoord;
+const gravity = 0.05;
+const playerHitboxWidth = 50
+const playerHitboxheight = 45
 
-//player var
-const spawnpointX = xCenterCoord
-const spawnpointY = yCenterCoord
-const gravity = 0.05
-
-let chosenCharacter = "Owl.png"
+//Default character
+let chosenCharacter = "goose.png";
 
 //Platform var
-const platformVelocityX = -0.5
-const spawnpointXDiviation = 200
-const spawnpointYDiviation = 20
+const platformDefaultSpawnLevel = yCenterCoord + 100;
+const platformVelocityX = -0.5;
+const spawnpointXDiviation = 200;
+const spawnpointYDiviation = 20;
+const platformHitboxWidth = 100
+const platformHitboxHeight= 30
 
+// Objects
 
-let player = new Player("player", "red", 50, 50, xCenterCoord, yCenterCoord - 100, gravity, -3, 0, 2, "Owl.png")
-let platform1 = new Platform("platform", "blue", 100, 30, xCenterCoord, platformDefaultSpawnLevel)
-let platform2 = new Platform("platform", "blue", 100, 30, xCenterCoord + 200, platformDefaultSpawnLevel)
-let platform3 = new Platform("platform", "blue", 100, 30, xCenterCoord + 400, platformDefaultSpawnLevel )
-let platform4 = new Platform("platform", "blue", 100, 30, xCenterCoord + 600, platformDefaultSpawnLevel)
-let platform5 = new Platform("platform", "blue", 100, 30, xCenterCoord + 800, platformDefaultSpawnLevel)
-let platform6 = new Platform("platform", "blue", 100, 30, xCenterCoord + 1000, platformDefaultSpawnLevel)
+let player = new Player(
+  playerHitboxWidth,
+  playerHitboxheight,
+  xCenterCoord,
+  yCenterCoord - 100,
+  gravity,
+  -3,
+  0,
+  2,
+  chosenCharacter
+);
+let platform1 = new Platform(
+  platformHitboxWidth,
+  platformHitboxHeight,
+  xCenterCoord,
+  platformDefaultSpawnLevel
+);
+let platform2 = new Platform(
+  platformHitboxWidth,
+  platformHitboxHeight,
+  xCenterCoord + 200,
+  platformDefaultSpawnLevel
+);
+let platform3 = new Platform(
+  platformHitboxWidth,
+  platformHitboxHeight,
+  xCenterCoord + 400,
+  platformDefaultSpawnLevel
+);
+let platform4 = new Platform(
+  platformHitboxWidth,
+  platformHitboxHeight,
+  xCenterCoord + 600,
+  platformDefaultSpawnLevel
+);
+let platform5 = new Platform(
+  platformHitboxWidth,
+  platformHitboxHeight,
+  xCenterCoord + 800,
+  platformDefaultSpawnLevel
+);
+let platform6 = new Platform(
+  platformHitboxWidth,
+  platformHitboxHeight,
+  xCenterCoord + 1000,
+  platformDefaultSpawnLevel
+);
 
-const platforms = [platform1, platform2, platform3, platform4, platform5, platform6]
+const platforms = [
+  platform1,
+  platform2,
+  platform3,
+  platform4,
+  platform5,
+  platform6,
+];
 
-function restartGame(){
-    player.posX = spawnpointX
-    player.posY = spawnpointY - 100
-    player.gravity = gravity
-    player.gravitySpeed = 0
-    player.speedY = 0
-    player.numOfJumps = 0
-    player.score = 0
-    displayScore()
+// menu js
+const goose = document.getElementById("goose");
+const owl = document.getElementById("owl");
+const eagle = document.getElementById("eagle");
+const swan = document.getElementById("swan");
 
-    isGameOver = false
-    isOnGround = false
-
-    randomizePlatformPos()
+function chooseCharacter(selectedCharacter) {
+  chosenCharacter = selectedCharacter.getAttribute("src");
 }
 
-function displayScore(){
-    scoreDiv.innerHTML = "Score: " + player.score
+//game js
+
+function restartGame() {
+  player.posX = spawnpointX;
+  player.posY = spawnpointY - 100;
+  player.gravity = gravity;
+  player.gravitySpeed = 0;
+  player.speedY = 0;
+  player.numOfJumps = 0;
+  player.score = 0;
+  displayScore();
+
+  isGameOver = false;
+
+  randomizePlatformPos();
 }
 
-function displayhighscore(){
-    highscoreDiv.innerHTML = "Highscore " + highscore
+function displayScore() {
+  scoreDiv.innerHTML = "Score: " + player.score;
 }
 
-function randomizePlatformPos(){
-    for (let i = 0; i < platforms.length; i++) {
-        let plat = platforms[i];
-    
-        plat.posX = spawnpointX + spawnpointXDiviation * i
-        plat.posY = generateNewPosY()
-        plat.speedX = platformVelocityX
+function displayHighscore() {
+  highscoreDiv.innerHTML = "Highscore: " + highscore;
+}
+
+function randomizePlatformPos() {
+  for (let i = 0; i < platforms.length; i++) {
+    let plat = platforms[i];
+
+    plat.posX = spawnpointX + spawnpointXDiviation * i;
+    plat.posY = generateNewPosY();
+    plat.speedX = platformVelocityX;
+    plat.isGivenPoint = false;
+  }
+}
+
+function drawEntity() {
+  let image = new Image();
+  image.src = player.image;
+
+  context.drawImage(
+    image,
+    player.posX,
+    player.posY,
+    player.width,
+    player.height
+  );
+
+  for (let i = 0; i < platforms.length; i++) {
+    let plat = platforms[i];
+
+    context.drawImage(
+      platformImage,
+      plat.posX,
+      plat.posY,
+      plat.width,
+      plat.height
+    );
+
+    if (plat.posX < xCenterCoord && plat.isGivenPoint == false) {
+      player.score += 1;
+      displayScore();
+      plat.isGivenPoint = true;
     }
+  }
 }
 
-function drawPlatforms(){
-    for (let i = 0; i < platforms.length; i++) {
-        let plat = platforms[i];
-        
-        context.fillStyle = plat.color
-        context.fillRect(plat.posX, plat.posY, plat.width, plat.height)
-    }
+//Clears screen every frame
+function clearScreen() {
+  context.drawImage(img, 0, 0);
 }
 
+function gameOver() {
+  player.gravity = 0;
+  player.gravitySpeed = 0;
+  player.speedY = 0;
 
-    function drawPlayer(){
-        let image = new Image()
-        image.src = player.image
-        var canvas = document.querySelector("canvas");
-    
-        var ctx = canvas.getContext("2d");
-    
-        ctx.drawImage(image, player.posX, player.posY, 50, 50)
-    }
+  isGameOver = true;
 
-function clearScreen(){
-    context.drawImage(img, 0, 0)
-    //context.fillRect(0, 0, canvas.width, canvas.height)
-}
+  scoreDiv.innerHTML = "GAME OVER";
 
+  if (player.score > highscore) {
+    highscore = player.score;
+    displayHighscore();
+  }
 
-function gameOver(){
-    player.gravity = 0
-    player.gravitySpeed = 0
-    player.speedY = 0
-
-    isGameOver = true
-
-    if(player.score > highscore){
-        highscore = player.score
-        displayhighscore()
-    }
-
-    for (let k = 0; k < platforms.length; k++) {
-        const plat = platforms[k];
-        plat.speedX = 0
-    }
-
+  for (let k = 0; k < platforms.length; k++) {
+    const plat = platforms[k];
+    plat.speedX = 0;
+  }
 }
 
 function collisionDetection() {
-
-        for (let r = 0; r < platforms.length; r++) {
-            
-            const b = platforms[r];
-            if(player.posX > b.posX - player.width && player.posX < b.posX + b.width && player.posY + player.height > b.posY + 5 && player.posY < b.posY + b.height){
-                gameOver()
-            }
-            else if (player.posX > b.posX - player.width && player.posX < b.posX + b.width && player.posY + player.height > b.posY && player.posY < b.posY + b.height) {
-                player.gravitySpeed = 0
-                player.speedY = 0
-                player.posY = b.posY - player.height
-                player.numOfJumps = 0
-                player.image = "Owl.png"
-            }
-
-        }
-
-        if(player.posY + player.height > groundLevel || player.posY < roofLevel){
-            gameOver()
-        }
-
-        
-
-}
-
-function generateNewPos(plat){
-    let heightDiviation = 150 * Math.random()
-    plat.posX = canvas.width + 100
-    plat.posY = 200 + heightDiviation
-
-    player.score += 1
-    displayScore()
-}
-
-function generateNewPosY(){
-    let heightDiviation = 150 * Math.random()
-    let newPosY = 200 + heightDiviation
-
-    return newPosY
-}
-
-function updatePos(){
-    player.gravitySpeed += player.gravity
-    player.posY += player.speedY + player.gravitySpeed
-
-}
-
-function updatePlatformPos(){
-
-
-    for (let c = 0; c < platforms.length; c++) {
-        let plat = platforms[c];
-        plat.posX += plat.speedX
-    
-        if(plat.posX < endOfScreen){
-            generateNewPos(plat)
-        }
+  for (let r = 0; r < platforms.length; r++) {
+    const b = platforms[r];
+    if (
+      player.posX > b.posX - player.width &&
+      player.posX < b.posX + b.width &&
+      player.posY + player.height > b.posY + 5 &&
+      player.posY < b.posY + b.height
+    ) {
+      gameOver();
+    } else if (
+      player.posX > b.posX - player.width &&
+      player.posX < b.posX + b.width &&
+      player.posY + player.height > b.posY &&
+      player.posY < b.posY + b.height
+    ) {
+      player.gravitySpeed = 0;
+      player.speedY = 0;
+      player.posY = b.posY - player.height;
+      player.numOfJumps = 0;
+      player.image = chosenCharacter;
     }
+  }
 
-}   
-
-function jump(){
-    if(player.numOfJumps < 2 && isGameOver === false){
-        player.gravitySpeed = 0
-        player.speedY = player.jumpSpeed
-        player.numOfJumps += 1
-        player.image = "flyingOwl.png"
-    }
+  if (player.posY + player.height > groundLevel || player.posY < roofLevel) {
+    gameOver();
+  }
 }
 
+function generateNewPos(plat) {
+  newPosY = generateNewPosY();
+  plat.posY = newPosY;
+  plat.posX = canvas.width + 100;
+  plat.isGivenPoint = false;
+}
+
+function generateNewPosY() {
+  let heightDiviation = 150 * Math.random();
+  let newPosY = 200 + heightDiviation;
+  return newPosY;
+}
+
+function updateEntityPos() {
+  player.gravitySpeed += player.gravity;
+  player.posY += player.speedY + player.gravitySpeed;
+
+  for (let c = 0; c < platforms.length; c++) {
+    let plat = platforms[c];
+    plat.posX += plat.speedX;
+
+    if (plat.posX < endOfScreen) {
+      generateNewPos(plat);
+    }
+  }
+}
+
+function jump() {
+  if (player.numOfJumps < 2 && isGameOver === false) {
+    player.gravitySpeed = 0;
+    player.speedY = player.jumpSpeed;
+    player.numOfJumps += 1;
+    player.image = "flying" + chosenCharacter;
+  }
+}
 
 document.onkeydown = function (e) {
-    const key = e.key;
-    switch (key) {
-      case "w":
-        jump()
-        
-        break;
-    }
-  };
+  const key = e.key;
+  switch (key) {
+    case "w":
+      jump();
 
-function update(){
-    clearScreen()
-    updatePos()
-    drawPlayer()
+      break;
+  }
+};
 
-    drawPlatforms()
-    updatePlatformPos()
-    collisionDetection()
+
+// Main Update Function
+function update() {
+  clearScreen();
+  drawEntity();
+  updateEntityPos();
+  collisionDetection();
 }
 
-function startGame(){
-    randomizePlatformPos()
-    setInterval(update, 10)
+function startGame() {
+  menu.style.display = "none";
+  game.style.display = "block";
+
+  randomizePlatformPos();
+  setInterval(update, 10);
 }
- 
-
-startGame()
-
--- startMenu.js
+++ startMenu.js
@@ -1,5 +1,25 @@
-const title = document.createElement("h1")
-const game = document.getElementById("game")
-title.innerHTML("Mega Bird")
-document.game.appendChild(title)
-title.style.color = "red"
+const goose = document.getElementById("goose")
+const owl = document.getElementById("owl")
+const eagle = document.getElementById("eagle")
+const swan = document.getElementById("swan")
+
+function chooseCharacter(selectedCharacter){
+    
+    console.log(selectedCharacter.getAttribute("src"))
+    
+    sessionStorage.setItem("character", selectedCharacter)
+}
+
+function chooseCharacter (selectedCharacter) {
+    // (A) VARIABLES TO PASS
+    let character = selectedCharacter
+  
+    // (B) URL PARAMETERS
+    let params = new URLSearchParams();
+    params.append("character", character);
+  
+    // (C) GO!
+    let url = "1b-query.html?" + params.toString();
+    location.href = url;
+
+}
